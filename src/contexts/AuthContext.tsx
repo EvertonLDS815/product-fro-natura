fa1a9bc23d4d0402281a35a71455134f86f119cc
EvertonLDS815 <evertonlino1234@gmail.com>
2023-11-29 13:00:12 -0300

    feat: config api

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,9 +1,13 @@
 import {createContext, ReactNode, useState} from 'react';
 
+import {destroyCookie} from 'nookies';
+import Router from 'next/router';
+
 type AuthContextData = {
     user?: UserProps;
     isAuthenticated: boolean;
     signIn: (credentials: SignInProps) => Promise<void>;
+    signOut: () => void;
 }
 
 type UserProps = {
@@ -22,17 +26,26 @@ type AuthProviderProps = {
 }
 export const AuthContext = createContext({} as AuthContextData);
 
+export function signOut() {
+    try {
+        destroyCookie(undefined, '@nextauth.token');
+        Router.push('/');
+    } catch (err) {
+        console.log('Erro ao deslogar');
+    }
+}
+
 export function AuthProvider({children}: AuthProviderProps) {
     const [user, setUser] = useState<UserProps>();
     const isAuthenticated = !!user;
     
     
-    async function signIn() {
-        alert("Clicou no login!")
+    async function signIn({email, password}: SignInProps) {
+        alert(`Seu email: ${email}, sua senha ${password}`)
     }
     
     return (
-        <AuthContext.Provider value={{user, isAuthenticated, signIn}}>
+        <AuthContext.Provider value={{user, isAuthenticated, signIn, signOut}}>
             {children}
         </AuthContext.Provider>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useContext } from 'react';
+import { FormEvent, useContext, useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../../styles/Home.module.scss';
@@ -13,12 +13,17 @@ import {AuthContext} from '../contexts/AuthContext';
 export default function Home() {
   const {signIn} = useContext(AuthContext);
 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [loading, setLoading] = useState(false);
+
   async function handleLogin(event: FormEvent) {
     event.preventDefault();
 
     const data = {
-      email: 'everton@gmail.com',
-      password: '123123'
+      email,
+      password
     }
     await signIn(data);
   }
@@ -34,8 +39,17 @@ export default function Home() {
 
         <div className={styles.login}>
           <form onSubmit={handleLogin}>
-            <Input type="text" placeholder={'Digite seu email...'} />
-            <Input type="password" placeholder={'Digite sua senha...'} />
+            <Input 
+              type="text" 
+              placeholder={'Digite seu email...'} 
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input 
+              type="password" 
+              placeholder={'Digite sua senha...'} 
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}/>
             <Button 
               type="submit"
               loading={false}

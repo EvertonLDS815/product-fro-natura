ffa674e9ad7fa873e67ff79d5a12634ee479581c
EvertonLDS815 <evertonlino1234@gmail.com>
2023-11-29 11:59:27 -0300

    feat: create AuthContext

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
 import '../../styles/global.scss'
 import type { AppProps } from 'next/app';
+import {AuthProvider} from '../contexts/AuthContext';
 
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <AuthProvider>
       <Component {...pageProps} />
+    </AuthProvider>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useContext } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../../styles/Home.module.scss';
@@ -7,7 +8,21 @@ import { Input } from '../components/ui/input';
 import {Button} from '../components/ui/button';
 import Link from 'next/link';
 
+import {AuthContext} from '../contexts/AuthContext';
+
 export default function Home() {
+  const {signIn} = useContext(AuthContext);
+
+  async function handleLogin(event: FormEvent) {
+    event.preventDefault();
+
+    const data = {
+      email: 'everton@gmail.com',
+      password: '123123'
+    }
+    await signIn(data);
+  }
+
   return (
     <>
       <Head>
@@ -18,7 +33,7 @@ export default function Home() {
         <Image src={Logo} alt="logo do site" />
 
         <div className={styles.login}>
-          <form>
+          <form onSubmit={handleLogin}>
             <Input type="text" placeholder={'Digite seu email...'} />
             <Input type="password" placeholder={'Digite sua senha...'} />
             <Button 
